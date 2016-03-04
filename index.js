9295a8e4a631a200cea114ca933007979d1a959a
khai96_ <hvksmr1996@gmail.com>
2016-03-04 09:49:10 +0700

    add generator

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
 	'use strict';
 
 	module.exports = {
-		function: require('./function.js'),
-		utils: require('./utils')
+		'function': require('./function.js'),
+		'generator': require('./generator.js'),
+		'utils': require('./utils')
 	};
 
 })(module);

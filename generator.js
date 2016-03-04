8c0594076e9a6671172fd2a2f026379c3c60938f
khai96_ <hvksmr1996@gmail.com>
2016-03-04 21:09:28 +0700

    complete generator.js

--- generator.js
+++ generator.js
@@ -2,6 +2,31 @@
 ((module) => {
 	'use strict';
 
-	
+	var serial = require('./utils/gserial.js');
+
+	var create = (...args) => {
+
+		var call = serial(...args);
+
+		return act;
+
+		function * act(...args) {
+			for (let element of call(...args, act, call)) {
+				yield * getValue(element);
+			}
+		}
+
+	}
+
+	function * getValue(object) {
+		if (object) {
+			let result = object.value;
+			if (object.deep) {
+				yield * result;
+			} else {
+				yield result;
+			}
+		}
+	}
 
 })(module);
